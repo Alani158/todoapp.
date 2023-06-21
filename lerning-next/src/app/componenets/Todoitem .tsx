db5990c572ec91f083115191be50f834abc39519
Alani Mujeeb <109475364+Alani158@users.noreply.github.com>
2023-06-21 19:18:48 +0100

    Add files via upload

--- lerning-next/src/app/componenets/Todoitem .tsx
+++ lerning-next/src/app/componenets/Todoitem .tsx
@@ -9,8 +9,9 @@ type TodoItemProps = {
 
 export function Todoitem({id,title,complete,toggleTodo}: TodoItemProps ){
      return(
-        <li className="flex gap-1 items-center">
-            <input id={id} type='checkbox' className="cursor-pointer peer"
+        <li className="flex gap-1 w-80 h-10 container items-center italic  bg-slate-300 border rounded mb-5">
+            
+            <input id={id} type='checkbox' className="cursor-pointer peer w-auto  italic font-bold"
        defaultChecked={complete}  onChange={e=>toggleTodo(id,e.target.checked)}   />
             <label htmlFor={id} className="peer-checked:line-through cursor-pointer">{title}</label>
         </li>
