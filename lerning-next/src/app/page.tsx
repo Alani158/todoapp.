eec531ebdb16a9c3177f43545ac6e68b57f34183
Alani Mujeeb <109475364+Alani158@users.noreply.github.com>
2023-06-21 19:17:22 +0100

    Add files via upload

--- lerning-next/src/app/page.tsx
+++ lerning-next/src/app/page.tsx
@@ -12,19 +12,28 @@ export default async function Home() {
 
 async function toggleTodo(id:string,complete:boolean){
   "use server"
-  console.log(id,complete)
+  await prisma.todo.update({where: {id},data: {complete} } )
+
 }
 const todos = await GetTodos()
 
   return (
-    <><header className=' justify-between flex m-4'>
-      <h1 className='text-2xl'>Todos</h1>
+    
+    <> <div className=' m-auto fixed h-20 bg-slate-700 inset-x-0 '>
+      <header className=' justify-between flex m-4  '>
+      <h1 className='text-2xl '>Todos</h1>
        <a href='/new' className='text-base border-2  rounded-md pl-2 pr-2   pt-1 pb-1 text-center hover:bg-slate-700'>
         New</a>
     </header>
-    <ul className='text-2xl'>{todos.map(todo=>(
+    
+    </div>
+   <div className=' justify-center flex '> 
+    <ul className='text-2xl m-auto  items-center mt-10 '>{todos.map(todo=>(
+      
+
       <Todoitem key={todo.id} {...todo} toggleTodo={toggleTodo}/>
     ))}</ul>
+    </div>
     </>
   )
 }
